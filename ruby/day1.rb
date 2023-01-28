ea3efdbc02dcf1b94e53ddb13a1ba78303b0a90b
James Trew <j.trew10@gmail.com>
2023-01-28 17:59:50 -0500

    day1

---
+++ ruby/day1.rb
@@ -0,0 +1,27 @@
+puts "Hello, world."
+str = "Welcome to Javascript!"
+str["Javascript"] = "Ruby"
+puts str
+str = "Welcome to Javascript!"
+str[8..20] = "Ruby"
+
+str = "Welcome to PHP Essentials!"
+puts str.gsub("PHP", "Ruby")
+str.gsub!("PHP", "Ruby")
+puts str
+
+puts "Hello, Ruby".index('Ruby')
+
+for i in 1..10 do
+  puts "#{i} James"
+end
+
+puts 'Pick a number for 1 to 10'
+guess = gets
+num = rand(10) + 1
+
+if guess == num
+  puts "You guessed right (#{num})"
+else
+  puts "You guessed wrong (#{num})"
+end
